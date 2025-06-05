7e36e8644eab5f2866527185a6871db37ed593c7
jeffli2002 <jefflee2002@gmail.com>
2025-06-05 22:59:45 +0800

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { LanguageProvider } from '@/store/index.js';
 import Layout from '@/components/layout/Layout';
 // 导入刘建毛草字体 (类似王羲之行书风格)
 import '@fontsource/liu-jian-mao-cao';
+// 导入 Vercel Analytics
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   // 强制样式重新计算
@@ -20,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <LanguageProvider>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </LanguageProvider>
   );
